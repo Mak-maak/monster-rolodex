19f70b6106413308bba60f373679bbf077b71626
Maak-mak <azizk7693@gmail.com>
2019-10-13 01:37:23 +0500

    Updates

---
+++ precache-manifest.53f1826f275828e60784d8a7fc5653a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5c04084554a58d08795f1cbf016ca88",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "f6dc067620e16ac2a968",
+    "url": "/monster-rolodex/static/css/main.0e409004.chunk.css"
+  },
+  {
+    "revision": "0b9227135eb05e7a2f42",
+    "url": "/monster-rolodex/static/js/2.cdb902b1.chunk.js"
+  },
+  {
+    "revision": "f6dc067620e16ac2a968",
+    "url": "/monster-rolodex/static/js/main.6090da92.chunk.js"
+  },
+  {
+    "revision": "95bc63e6bf147c877795",
+    "url": "/monster-rolodex/static/js/runtime-main.d92cc834.js"
+  }
+]);
